17206eb4c84efe1efd0af4e6b9fd2abfacbdcbbe
Tarun Sen Behera <65290279+tarunbehera7@users.noreply.github.com>
2023-12-21 20:45:06 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,22 @@
+
+////===== Default-Export =====
+    // import ui from './module2.mjs'
+    // console.log(ui);
+    //// O/P - Vinay
+
+////=====  Named-Export  =====
+    // import dza from './module2.mjs'
+    // console.log(dza);
+    //// O/P- Vinay
+
+// Use Curly braces only when ur 
+//     Not exporting sthg by default.. 
+import dza, {a, c, d} from './module2.mjs'
+console.log(dza);
+console.log(c);
+console.log(d);
+console.log(a);
+// O/P - Vinay
+//       Shubham
+//       Abhishek
+//       Tarun
---
+++ module2.mjs
@@ -0,0 +1,9 @@
+const a =  "Tarun";
+const b =  "Vinay";
+const c =  "Shubham";
+const d =  "Abhishek";
+
+export default b;
+export {a};
+export {c};
+export {d};
