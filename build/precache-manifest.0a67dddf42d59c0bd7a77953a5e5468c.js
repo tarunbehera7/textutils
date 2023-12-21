7cd296a78c36117b3b08b0a54ffcfc7c4462b66c
Tarun Sen Behera <65290279+tarunbehera7@users.noreply.github.com>
2023-12-21 14:43:26 +0530

    Add files via upload

---
+++ build/precache-manifest.0a67dddf42d59c0bd7a77953a5e5468c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0156b8695062d0a8ea704de859c65f",
+    "url": "/textutils/index.html"
+  },
+  {
+    "revision": "b458ee3d3e6cc6d7e3fc",
+    "url": "/textutils/static/css/main.007c0bea.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/textutils/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/textutils/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "b458ee3d3e6cc6d7e3fc",
+    "url": "/textutils/static/js/main.99ac388a.chunk.js"
+  },
+  {
+    "revision": "7a8be6afc65844edb6f3",
+    "url": "/textutils/static/js/runtime~main.0b59052a.js"
+  }
+]);
